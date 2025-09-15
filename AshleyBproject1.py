0f6030c21c63188e96b752c80c13eb0d6e70b1e8
Ashley Bercy <yhelissa79@gmail.com>
2025-09-15 10:19:35 -0400

    project 1

---
+++ AshleyBproject1.py
@@ -0,0 +1,6 @@
+credits_now = 14
+credits_before = 0
+credits_taken = credits_before + credits_now
+first_major = "cybersecurity"
+credits_to_go = 120 - credits_taken
+print(f"for my {first_major} major, I need {credits_to_go} more credits to graduate")
